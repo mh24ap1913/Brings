24491ef5d0b00723351cf222283404e0cc7b8bf5
Ghodkevikas328@gmail.com <admin@192.168.0.19>
2019-09-19 10:06:43 +0530

    new project

---
+++ JDBCApp/src/in/co/brings/JDBCConnectionDemo.java
@@ -0,0 +1,37 @@
+package in.co.brings;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCConnectionDemo {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","root");
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery("select * from student");
+		while(rs.next()) {
+			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+			
+		
+		/*
+		PreparedStatement pstmt=con.prepareStatement("insert into student values(?,?,?)");
+		pstmt.setInt(1, 10);
+		pstmt.setString(2, "vishal");
+		pstmt.setString(3, "pune");
+		int res=pstmt.executeUpdate();
+		if(res==0) {
+			System.out.println("value not  store successfully");
+		}
+		else {
+			System.out.println("value saved successfully");
+		}*/
+		
+		}
+		System.out.println("value retrive succesfully");
+	}
+
+
+}
