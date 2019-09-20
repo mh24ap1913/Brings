53291dbbc74c6d3993aef672cda1600ce2e0cc95
Ghodkevikas328@gmail.com <admin@192.168.0.7>
2019-09-20 23:37:47 +0530

    sorted order by id

--- JDBCApp/src/in/co/brings/JDBCConnectionDemo.java
+++ JDBCApp/src/in/co/brings/JDBCConnectionDemo.java
@@ -11,7 +11,7 @@ public class JDBCConnectionDemo {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","root");
 		Statement stmt=con.createStatement();
-		ResultSet rs=stmt.executeQuery("select * from student");
+		ResultSet rs=stmt.executeQuery("select * from student order by id");
 		while(rs.next()) {
 			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
 			
