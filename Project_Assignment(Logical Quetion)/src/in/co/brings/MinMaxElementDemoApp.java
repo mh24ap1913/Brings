b85bee598b989e63af09168c21ae4d0f7be38e34
Ghodkevikas328@gmail.com <admin@admin-PC>
2019-09-21 22:59:52 +0530

    Min and max value given by array

---
+++ Project_Assignment(Logical Quetion)/src/in/co/brings/MinMaxElementDemoApp.java
@@ -0,0 +1,39 @@
+package in.co.brings;
+
+public class MinMaxElementDemoApp {
+	
+	
+	public static void main(String args[]){
+	    int array[] = new int[]{1, 2, 5, 5, 6, 6, 7, 2};
+	 
+	    // Calling getMax() method for getting max value
+	    int max = getMax(array);
+	    System.out.println("Maximum Value is: "+max);
+	 
+	    // Calling getMin() method for getting min value
+	    int min = getMin(array);
+	    System.out.println("Minimum Value is: "+min);
+	  }
+	 
+	  // Method for getting the maximum value
+	  public static int getMax(int[] inputArray){ 
+	    int maxValue = inputArray[0]; 
+	    for(int i=1;i < inputArray.length;i++){ 
+	      if(inputArray[i] > maxValue){ 
+	         maxValue = inputArray[i]; 
+	      } 
+	    } 
+	    return maxValue; 
+	  }
+	 
+	  // Method for getting the minimum value
+	  public static int getMin(int[] inputArray){ 
+	    int minValue = inputArray[0]; 
+	    for(int i=1;i<inputArray.length;i++){ 
+	      if(inputArray[i] < minValue){ 
+	        minValue = inputArray[i]; 
+	      } 
+	    } 
+	    return minValue; 
+	  } 
+}
